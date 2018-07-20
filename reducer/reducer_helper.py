d7be24e0a41e93619a04d6aa31704e495755642c
Max Buck <maxwellhigginsbuck@gmail.com>
2018-07-20 12:43:50 -0400

    initialize

---
+++ reducer/reducer_helper.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from time import sleep
+import json
+
+def get_reduction(data):
+    sleep(.1)
+    # look at the first character
+    return [json.loads(data)['data'][0]]
